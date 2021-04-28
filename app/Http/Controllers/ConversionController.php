824825c9e1df29cd99ae382e0c7596f54647bd55
Jiří Nedorost <jiri@nedorost>
2021-04-28 17:27:47 +0200

    Add currency conversion to frontend

--- app/Http/Controllers/ConversionController.php
+++ app/Http/Controllers/ConversionController.php
@@ -3,28 +3,44 @@
 namespace App\Http\Controllers;
 
 use App\Models\Currencies;
+use App\Services\ConverterService;
 use Illuminate\Validation\Rule;
 use Illuminate\Http\Request;
 
 class ConversionController extends Controller
 {
     private $currencies;
+    private $converter;
 
-    public function __construct(Currencies $currencies)
+    public function __construct(Currencies $currencies, ConverterService $converter)
     {
         $this->currencies = $currencies;
+        $this->converter = $converter;
     }
 
     public function index()
     {
         $allCurrencies = $this->currencies->pluck('combined_name')->all(); //get all currencies from DB
 
-        return view('index', [
-            'currencies' => $allCurrencies,
-            'mostConvertedCurr' => 'Test',
-            'totalConversions' => 1,
-            'totalConverted' => 2,
-        ],);
+        $displayData = [];
+        if (session()->get('redir')) {
+            $amount = session()->get('amount');
+            $from = session()->get('from');
+            $to = session()->get('to');
+
+            $convertedAmount = $this->converter->convertCurrency($from, $to, $amount);
+            $displayData = ['convertedAmount' => $convertedAmount, 'convertedTo' => $to, 'originalAmount' => $amount, 'convertedFrom' => $from,];
+        }
+
+        return view('index', array_merge(
+            [
+                'currencies' => $allCurrencies,
+                'mostConvertedCurr' => 'Test',
+                'totalConversions' => 1,
+                'totalConverted' => 2,
+            ],
+            $displayData
+        ));
     }
 
     public function convert(Request $request)
--- app/Services/ConverterService.php
+++ app/Services/ConverterService.php
@@ -2,7 +2,49 @@
 
 namespace App\Services;
 
+use App\Models\Currencies;
+use App\Services\CacheService;
+
 class ConverterService
 {
-    //
+    /**
+     * CacheService object
+     * @var cache
+     */
+    private $cache;
+
+    /**
+     * Currencies DB model
+     * @var currencies
+     */
+    private $currencies;
+
+    public function __construct(CacheService $cache, Currencies $currencies)
+    {
+        $this->cache = $cache;
+        $this->currencies = $currencies;
+    }
+
+    /**
+     * Converts Specified amount from one currency to another
+     * All currencies are coverted using USD as base
+     * 
+     * @param string from Requires specific format as saved in table Currencies - combined_name column
+     * @param string to Requires specific format as saved in table Currencies - combined_name column
+     * @param float amount
+     * 
+     * @return float
+     */
+    public function convertCurrency(string $from, string $to, float $amount): float
+    {
+        //TODO checking if currency exist, if not refresh all
+
+        $fromSymbol = $this->currencies->where('combined_name', $from)->first()->symbol;
+        $toSymbol = $this->currencies->where('combined_name', $to)->first()->symbol;
+
+        $rateFrom = $this->cache->getRate($fromSymbol);
+        $rateTo = $this->cache->getRate($toSymbol);
+
+        return ($amount/$rateFrom)*$rateTo;
+    }
 }
